f2168ca5ca10856e4af9fbf79f42adba4c9ebedb
Mike <mike.d.meier@gmail.com>
2013-04-14 08:30:21 -0600

    rewrite navigation - playback borked

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -3,10 +3,11 @@ NAME = "YTV"
 ART  = 'art-default.jpg'
 ICON = 'icon-default.png'
 
-BASE_URL = "http://www.ytv.com/videos/"
-YTV_PLAYER_KEY = "AQ~~,AAAAocwuf3E~,OkU0MlqQwrSzQO2jTf7KgSe64XrUuKHP"
-YTV_PLAYER_ID = "912499644001"
-AMF_URL = 'http://c.brightcove.com/services/messagebroker/amf'
+BASE_URL = "http://www.ytv.com"
+EXCLUSIONS = ['http://www.ytv.com/shows/98/big-fun-movies/kidschoiceweekend.aspx']
+
+NAMESPACES = {"plmedia" : "http://xml.theplatform.com/media/data/Media"}
+SMIL_NS = {"a" : "http://www.w3.org/2005/SMIL21/Language"}
 
 ####################################################################################################
 
@@ -22,14 +23,17 @@ def MainMenu():
     page = HTML.ElementFromURL(BASE_URL)
     categories = []
 
-    for category in page.xpath('//div[@id="video-navigation"]//a'):
-        title = category.text
-        url = category.get('href')
+    for category in page.xpath('//div[@class="mega-nav-item"]'):
+        title = category.xpath('./a')[1].text.strip()
+        url = category.xpath('./a')[1].get('href')
+        if url in EXCLUSIONS:
+            continue
+        thumb = category.xpath('.//img')[0].get('src')
         if  url == '/videos/': url = '?'
         if title.lower() in categories:
             continue
         else:
-            oc.add(DirectoryObject(key=Callback(CategoryPage, title=title, url=url), title=title))
+            oc.add(DirectoryObject(key=Callback(CategoryPage, title=title, url=url), title=title, thumb=Resource.ContentsOfURLWithFallback(thumb)))
             categories.append(title.lower())
 
     oc.objects.sort(key = lambda obj: obj.title)
@@ -40,66 +44,96 @@ def MainMenu():
 @route(VIDEO_PREFIX + "/category")
 def CategoryPage(title, url):
     oc = ObjectContainer(title2=title)
-
-    request_url = BASE_URL + 'videos.ashx' + url
-    results = JSON.ObjectFromURL(request_url)
-
-    for video in results:
-        title = video['Name']
-        summary = video['ShortDescription']
-        thumb = video['ThumbnailURL']
-        duration = (int(video['Duration'].split(":")[0])*60 + int(video['Duration'].split(":")[1]))*1000
-        videoId = video['Id']
-        Log(videoId)
-        url = "YTV_PLUGIN://videoID=%s" % videoId
-        oc.add(
-            VideoClipObject(
-                url = url,
-                title = title,
-                summary = summary,
-                duration = duration, 
-                thumb = Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON)
-            )
-        )
-
+    
+    if url.startswith('http://'):
+        request_url = url + 'watch.ashx'
+    else:
+        request_url = BASE_URL + url + 'watch.aspx'
+    
+    try:
+        Log(request_url)
+        data = HTML.ElementFromURL(request_url)
+        platform_url = data.xpath('//div[@class="video-container"]//iframe')[0].get('src')
+    except:
+        request_url = request_url.replace('/watch.aspx', '/videos.aspx')
+        Log(request_url)
+        data = HTML.ElementFromURL(request_url)
+        platform_url = data.xpath('//div[@class="video-container"]//iframe')[0].get('src')
+    
+    platform_data = HTML.ElementFromURL(platform_url)
+    feed_url = platform_data.xpath('//link[@rel="alternate"]')[0].get('href')
+    
+    feed = RSS.FeedFromURL(feed_url)
+    
+    for item in feed.entries:
+        url = item.link
+        Log(url)
+        title = item.title
+        Log(title)
+        summary = item.summary
+        Log(summary)
+        thumb = item.plmedia_defaultthumbnailurl
+        Log(thumb)
+        date = item.updated
+        Log(date)
+        duration = item.media_content[0]['duration']
+        Log(duration)
+        duration = int(float(duration)*1000)
+        Log(duration)
+        i = 0
+        resolutions = []
+        while i < len(item.media_content):
+            bitrate = int(item.media_content[i]['bitrate'].split('.')[0])
+            Log(bitrate)
+            resolution = item.media_content[i]['height']
+            Log(resolution)
+            url = item.media_content[i]['url']
+            Log(url)
+            resolutions.append({'url':url, 'res':resolution, 'bitrate':bitrate})
+            i += 1
+        
+        oc.add(CreateVideoClipObject(
+            url = url,
+            title = title,
+            summary = summary,
+            date = date,
+            thumb = thumb,
+            json_string = JSON.StringFromObject(resolutions)
+    ))
     return oc
 
-####################################################################################################
-def AmfRequest(playerID=None, playerKey=None, videoPlayer=None):
+def CreateVideoClipObject(url, title, summary, date, thumb, json_string, include_container=False):
+    items = []
+    for version in JSON.ObjectFromString(json_string):
+        items.append(
+            MediaObject(
+                parts = [PartObject(key=Callback(PlayVideo, url=version['url']))],
+                bitrate = version['bitrate'],
+                video_resolution = version['res'],
+                audio_channels = 2,
+                optimized_for_streaming = True
+                )
+            )
 
-    endpoint = AMF_URL
-    if playerKey:
-        endpoint += '?playerKey=%s' % playerKey
+    items.reverse()
 
-    client = AMF.RemotingService(url=endpoint, user_agent='', client_type=3)
-    service = client.getService('com.brightcove.experience.ExperienceRuntimeFacade')
+    videoclip_obj = VideoClipObject(
+        key = Callback(CreateVideoClipObject, url=url, title=title, summary=summary, date=date, thumb=thumb, json_string=json_string, include_container=True),
+        rating_key = url,
+        title = title,
+        summary = summary,
+        thumb = Resource.ContentsOfURLWithFallback(thumb),
+        items = items
+    )
 
-    AMF.RegisterClass(ContentOverride, 'com.brightcove.experience.ContentOverride')
-    AMF.RegisterClass(ViewerExperienceRequest, 'com.brightcove.experience.ViewerExperienceRequest')
-
-    video_obj = ContentOverride(videoPlayer)
-    experience = ViewerExperienceRequest(playerID, playerKey, video_obj)
-
-    return service.getDataForExperience('', experience)['programmedContent']['videoPlayer']['mediaDTO']
-
-####################################################################################################
-class ContentOverride(object):
-  def __init__ (self, videoPlayer=None):
-    self.contentType = int(0)
-    self.contentIds = None
-    self.target = 'videoPlayer'
-    self.contentId = int(videoPlayer)
-    self.featuredRefId = None
-    self.contentRefIds = None
-    self.featuredId = float('nan')
-    self.contentRefId = None
-
-class ViewerExperienceRequest(object):
-  def __init__ (self, playerID=None, playerKey=None, video_obj=None):
-    self.experienceId = int(playerID)
-    self.playerKey = playerKey
-    self.contentOverrides = []
-    self.contentOverrides.append(video_obj)
-    self.TTLToken = ''
-    self.URL = ''
-    self.deliveryType = float('nan')
+    if include_container:
+        return ObjectContainer(objects=[videoclip_obj])
+    else:
+        return videoclip_obj
+    
+def PlayVideo(url):
+    smil = XML.ElementFromURL(url + '&manifest=f4m&format=SMIL')
+    video_url = smil.xpath('//a:video', namespaces = SMIL_NS)[0].get('src')
+    headers = HTTP.Request(video_url).headers
+    Log(headers)
+    return Redirect(video_url)
